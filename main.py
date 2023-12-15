f5ffab32e191406462467dbe4bf4a3b3118edc59
Jan Menssen <j.menssen@me.com>
2023-12-15 10:19:23 +0100

    working MDI example

---
+++ main.py
@@ -0,0 +1,83 @@
+#
+# main.py
+#
+#     this is the main file for the FysioMon project. Visualisation and storage of physiological
+#     signals
+#
+# Modifications
+#     15-dec-2023  JM  initial version
+
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow,QMdiArea, QMdiSubWindow, QTextEdit, QMenu
+from PySide6.QtGui import QAction
+
+# define the MDI class
+
+class MDIWindow(QMainWindow) :
+
+  # constructor
+
+  def __init__(self) :
+    super().__init__()
+
+    self.count = 0
+
+    self.mdi = QMdiArea()
+    self.setCentralWidget(self.mdi)
+
+    # create the menu structure
+
+    bar = self.menuBar()
+    fileMenu = QMenu("&File", self)
+    bar.addMenu(fileMenu)
+
+    newAction = QAction("New",self)
+    newAction.triggered.connect(self.newPressed)
+    fileMenu.addAction(newAction)
+
+    cascadeAction = QAction("Cascade",self)
+    cascadeAction.triggered.connect(self.cascadePressed)
+    fileMenu.addAction(cascadeAction)
+
+    tiledAction = QAction("Tiled",self)
+    tiledAction.triggered.connect(self.tiledPressed)
+    fileMenu.addAction(tiledAction)
+
+    #-jm file.connect(self.WindowTrig)
+
+    self.setWindowTitle("MDI Application")
+
+
+  def newPressed(self) :
+
+    self.count = self.count + 1
+      
+    sub = QMdiSubWindow()
+    sub.setWidget(QTextEdit())
+    sub.setWindowTitle("Sub Windows " + str(self.count))
+
+    self.mdi.addSubWindow(sub)
+    sub.show()
+
+  def cascadePressed(self) :
+  
+    self.mdi.cascadeSubWindows()
+
+  def tiledPressed(self) :
+      
+    self.mdi.tileSubWindows()
+
+# main loop
+      
+if __name__ == '__main__' :
+
+  app = QApplication(sys.argv)
+  mdiwindow = MDIWindow()
+  mdiwindow.show()
+  sys.exit(app.exec())
+
+
+
+    
+
+    
