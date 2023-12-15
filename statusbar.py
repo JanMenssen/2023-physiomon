f5c4aec9e9488e5cdc22a5d4c90810b738ee36b7
Jan Menssen <j.menssen@me.com>
2023-12-15 12:20:24 +0100

    update as on dec 15h

--- statusbar.py
+++ statusbar.py
@@ -6,7 +6,10 @@
 #  modifications
 #     15-dec-2023   JM  initial version
 
-from PySide6.QtWidgets import QStatusBar
+from PySide6.QtWidgets import QWidget,  QStatusBar
+from PySide6.QtGui import QPainter, QPen
+from PySide6.QtCore import Qt 
+from datetime import datetime
 
 class myStatusBar(QStatusBar) :
   
@@ -14,12 +17,17 @@ class myStatusBar(QStatusBar) :
 
   def __init__(self) :
     super().__init__()
+    self.led = ledWidget()
+    self.addPermanentWidget(self.led)
 
   # setText
   #
   #     this method shows the text for the number of seconds
     
   def setText(self,txtToDisplay,timeToDisplay) :
+
+    currentTime = datetime.now().strftime("%H:%M:%S")
+    txtToDisplay = currentTime + " >>> " + txtToDisplay
     self.showMessage(txtToDisplay,(timeToDisplay * 1000))
 
 
