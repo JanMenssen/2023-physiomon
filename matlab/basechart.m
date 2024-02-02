87746cd406b609751c05ff6c39c3c5d34f5a37c4
Jan Menssen <j.menssen@me.com>
2024-02-02 16:22:21 +0100

    as of february 2nd

---
+++ matlab/basechart.m
@@ -0,0 +1,86 @@
+%
+% basechart
+%
+%     this is an abstract class for handling the displays. Children of this class are
+%
+%         - stripchart
+%         - scopechart
+%         - sweepchart
+%
+%   In the base chart the <downSampler is initialise, which reduces the number of points to
+%   plot in the display
+%
+% modifications
+%     02-feb-2024  JM initial version
+
+classdef basechart
+
+  properties(Access = private)
+  
+    m_channels = []               % list with channels that should be plotted in the display
+    m_handle = []                 % handle to the axis 
+    m_downSampler = [];           % downsample class
+    m_buffer = []                 % contains the data points (cell array
+    m_pntsInGraph = [];           % number of points in the graph
+    m_sampleRate = [500 500 500]; % sampleRate (Hz)
+  end
+
+  methods
+
+    %% constructor
+
+    function obj = basechart(axisHandle,channels)
+
+      % the constructor set the current axis handle which is used in the plotting and a
+      % list with channels that should be plotted into this chart
+      %
+      %     syntax : obj = basechart(axisHandle,channels)
+      %
+      % with <axisHandle> a handle that is created and channels a list of channels that
+      % should be plotted
+      
+      obj.m_handle = axisHandle;
+      obj.m_channels = channels;
+
+    end
+
+    %% setYaxis
+
+    function setYaxis(obj,ymin,ymax)
+
+      % setYaxis sets the limit of the y-axis
+      %
+      %     syntax : setAxis(obj,ymin,ymax)
+      %
+      % with <ymin> the lower boundary and <ymax> the upper boundary of the y-axis
+
+      obj.m_handle.yLim([ymin ymax]);
+
+    end
+
+    %% setTimeAxis
+
+    function setTimeAxis(obj,nsec)
+
+      % set the time scale
+
+      obj.m_handle.xLim([0 nsec]);
+      
+      % calculate the number of points in the graph
+
+      for ichan =1:length(obj.m_channels)
+      end
+      
+      % set the points for the x-axis, these can be calculated and plot NaN's for the
+      % yaxis and plot
+
+    end
+
+    %% finishUpdate
+
+    function finishUpdate(~)
+    end
+
+  end
+
+end
--- matlab/displays.m
+++ matlab/displays.m
@@ -9,6 +9,7 @@
 classdef displays
 
   properties (Access = private)
+    m_chart = [];
   end
 
   methods
@@ -19,8 +20,7 @@ classdef displays
     
       % constructor of the class, members are initialised
       %
-      %     syntax : obj = displays()
-
+      %     syntax : obj = displays(;
     end
 
     %% initialise
@@ -35,24 +35,77 @@ classdef displays
 
     %% configure
    
-    function obj = configure(obj,mySettings)
+    function obj = configure(obj,mySettings,cansvasHandle)
     
       % in configure, the displays are configured using the information from an instance
       % of the <physiomon_settings> class. The number of displays are placed on the canvas
       % on the desired place and scaling is done. 
       %
-      %     syntax : obj = configure(obj,mySettings)
+      %     syntax : obj = configure(obj,mySettings,canvasHandle)
       %
-      % with <mySettings> a "filled" instance of the <phsyiomon_settings> class
+      % with <mySettings> a "filled" instance of the <phsyiomon_settings> class and
+      % <canvasHandle> a handle to the canvas (figure)
+ 
+      % first remove the the exisiting charts
 
+      obj.m_chart = [];
+      axes = findobj(cansvasHandle,'type','axes');
+      delete(axes);
+
+      % now create each display on the canvas, but before we must known the size of the
+      % figure
+
+      canvasWidth = h.Position(3);
+      canvasHeight = h.Position(4);
+
+      for iDisp = 1:length(mySettings.m_displays)
+
+        curDisp = mySettings.m_displays(iDisp);
+
+        top = canvasWidth * curDisp.top;
+        left = canvasHeight * curDisp.left;
+        width = canvasWidth * curDisp.width;
+        height = canvasHeight * curDisp.height;
+
+        % get the channels which should be on the current display, currently
+        % max 3 channels are allowed
+
+        channels = zeros(3,1);
+        i = 1;
+        for iChan = 1:length(mySettings.m_channels)
+          if (mySettings.m_channels(iChan).display == iDisp)
+            channels(i) = iChan;
+            i = i + 1;
+          end     
+        end
+        channels((i-1):3) = [];
+
+        % and create the current display
+
+        handle = subplot('position',[top left width height]);
+        switch curDisp.mode
+          case 1
+            obj.m_chart(iDisp) = stripchart(handle,channels);
+          case 2
+            obj.m_chart(iDisp) = sweepchart(handle,channels);
+          case 3
+            obj.m_chart(iDisp) = scopeChart(handle,channels);
+        end
+
+        % and set the Y-axis and time axis for the current chart
+
+        chart.setYaxis(curDisp.ymin,curDisp.ymax);
+        chart.setTimeAxis(curDisp.timeScale);
+      
+      end
     end
 
     %% plot
 
     function myChannels = plot(obj,myChannels)
     
-      % using the new information from the buffers in the instance of the <physiomon_channels> 
-      % class (written by the read method of the instance of <measuredevice>, this method plots
+      % using the new data from the buffers in the instance of the <physiomon_channels> class 
+      % (written by the read method of the instance of <measuredevice>, this method plots
       % this new data on the charts, using the method thas is selected (strip, scope,
       % sweep or numeric)
       %
@@ -60,6 +113,28 @@ classdef displays
       %
       % with <myChannels> and instance of the <physiomon_channels> 
       
+      numDisp = length(obj.m_chart);
+      for iDisp = 1:numDisp
+      
+        % a display could have more channels, so we need the data for all channels in this
+        % display
+
+        for iChan = 1:length(obj.m_chart.m_channels)
+        
+          % get the channel number and get the data
+
+          curChannel = obj.m_chart.m_channels(iChan);
+          data = myChannels.readDisplay(curChannel);
+          
+          % update the display
+
+          obj.m_chart(iDisp).update(iChan,data);
+       
+        end
+        obj.m_chart.finishUpdate();
+      end
+
+      drawnow limitrate
     end
 
   end
